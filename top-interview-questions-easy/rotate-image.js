4bfed7c8b16c946bf2d39b1992a54d6443181f19
Dreamland <showd0wn.land@gmail.com>
2018-03-23 17:09:23 +0800

    update

--- top-interview-questions-easy/rotate-image.js
+++ top-interview-questions-easy/rotate-image.js
@@ -9,7 +9,7 @@ var rotate = function(matrix) {
   for (i = 0; i < n; i++) {
     for (j = 0; j < n; j++) {
       if (i >= j) continue
-      [matrix[i][j], matrix[j][i]]= [matrix[j][i],matrix[i][j]]
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
     }
   }
 }
