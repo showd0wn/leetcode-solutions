1753a3d081a62663883c1ee4f3322ccfc5e12d23
Dreamland <showd0wn.land@gmail.com>
2018-04-09 17:45:19 +0800

    up

--- top-interview-questions-easy/String/reverse-integer.js
+++ top-interview-questions-easy/String/reverse-integer.js
@@ -2,6 +2,12 @@
  * @param {number} x
  * @return {number}
  */
- var reverse = function(x) {
-
- }
+var reverse = function(x) {
+  var limit =  Math.pow(2, 31)
+  if (x > limit) return 0
+  var bool = x > 0
+  var array = String(Math.abs(x)).split('').reverse()
+  var number = Number(array.join(''))
+  if (number > limit) return 0
+  return bool ? number : 0 - number
+}
