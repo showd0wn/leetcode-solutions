b8d2081955a7eba3fc22c02b2121c75dedfe047b
Dreamland <showd0wn.land@gmail.com>
2018-04-12 17:31:26 +0800

    upo

---
+++ top-interview-questions-easy/Linked-List/reverse-linked-list.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+// iteratively
+var reverseList = function(head) {
+  var prev = null
+  while (head) {
+    var next = head.next
+    head.next = prev
+    prev = head
+    head = next
+  }
+  return prev
+};
+
+// recursively
+var reverseList = function(head) {
+  if (!head || !head.next) {
+      return head;
+  }
+  var newHead =  reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return newHead
+};
