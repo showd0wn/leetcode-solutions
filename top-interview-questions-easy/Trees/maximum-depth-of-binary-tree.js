f7abbd654c540579c5003307f00735c04bbecc0d
Dreamland <showd0wn.land@gmail.com>
2018-04-19 11:39:00 +0800

    up

--- top-interview-questions-easy/Trees/maximum-depth-of-binary-tree.js
+++ top-interview-questions-easy/Trees/maximum-depth-of-binary-tree.js
@@ -10,6 +10,6 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if (root === null || root === undefined) return 0
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+  if (!root) return 0
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
 };
---
+++ top-interview-questions-easy/Trees/validate-binary-search-tree.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  if (!root) return true
+  var helper = function(root, min, max) {
+    if (!root) return true
+    if ((min !== null && root.val <= min) || (max !== null && root.val >= max)) return false
+    return helper(root.left, min, root.val) && helper(root.right, root.val, max)
+  }
+  return helper(root, null, null)
+};
