16eb0fc61f538de17fe836fc3c1214d9a26197a0
Dreamland <showd0wn.land@gmail.com>
2018-05-02 15:47:12 +0800

    up

---
+++ top-interview-questions-easy/Sorting and Searching/merge-sorted-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  var len = m + n
+  m--
+  n--
+  while (len--) {
+    nums1[len] = (n < 0 || nums1[m] > nums2[n])
+      ? nums1[m--]
+      : nums2[n--]
+  }
+};
