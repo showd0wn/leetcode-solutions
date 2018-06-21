a980b3baf0398034d7dc68c69ae4b82053e04415
Dreamland <showd0wn.land@gmail.com>
2018-06-21 16:52:48 +0800

    up

--- top-interview-questions-easy/Array/remove-duplicates-from-sorted-array/26.py
+++ top-interview-questions-easy/Array/remove-duplicates-from-sorted-array/26.py
@@ -4,4 +4,8 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        
+        length = len(nums)
+        for i in range(1, length)[::-1]:
+            if (nums[i] == nums[i - 1]):
+                nums.pop(i)
+        return len(nums)
