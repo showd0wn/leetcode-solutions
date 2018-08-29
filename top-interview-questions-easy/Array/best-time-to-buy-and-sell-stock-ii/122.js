6088184536cba2e1737e33a0c276ee21f8a498e2
Dreamland <showd0wn.land@gmail.com>
2018-08-29 18:11:15 +0800

    up

--- top-interview-questions-easy/Array/best-time-to-buy-and-sell-stock-ii/122.js
+++ top-interview-questions-easy/Array/best-time-to-buy-and-sell-stock-ii/122.js
@@ -2,10 +2,10 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-  var maxProfit = 0
-  var length = prices.length
-  for (var i = 0; i < length - 1; i++) {
+const maxProfit = function(prices) {
+  const len = prices.length
+  let maxProfit = 0
+  for (let i = 0; i < len - 1; i++) {
     if (prices[i] < prices[i + 1]) {
       maxProfit += prices[i + 1] - prices[i]
     }
