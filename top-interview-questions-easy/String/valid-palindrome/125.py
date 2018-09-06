d579af40e27d1e12909fc0a0be4cd95ac7b1c930
Dreamland <showd0wn.land@gmail.com>
2018-09-06 14:13:23 +0800

    up

---
+++ top-interview-questions-easy/String/valid-palindrome/125.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = ''.join(e for e in s if e.isalnum()).lower()
+        return s == s[::-1]
