f7de58fb9f80856b9d029d0c298d42c96431dc1e
Dreamland <showd0wn.land@gmail.com>
2018-09-17 11:34:18 +0800

    up

--- top-interview-questions-easy/Others/pascal's-triangle/118.py
+++ top-interview-questions-easy/Others/pascal's-triangle/118.py
@@ -4,3 +4,9 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
+        result = [[]] * numRows
+        for i in range(numRows):
+            result[i] = [1] * (i + 1)
+            for j in range(1, i):
+                result[i][j] = result[i - 1][j - 1] + result[i - 1][j]
+        return result
