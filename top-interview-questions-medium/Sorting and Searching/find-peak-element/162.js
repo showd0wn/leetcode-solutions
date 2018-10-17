7f79598caa2a1ec2c6bfa7d0523ff81dd4072cfe
Dreamland <showd0wn.land@gmail.com>
2018-10-17 13:22:27 +0800

    up

--- top-interview-questions-medium/Sorting and Searching/find-peak-element/162.js
+++ top-interview-questions-medium/Sorting and Searching/find-peak-element/162.js
@@ -6,7 +6,7 @@ var findPeakElement = function(nums) {
   let min = 0
   let max = nums.length - 1
   for (let mid = Math.floor(max / 2); min < max; mid = Math.floor((min + max) / 2)) {
-    if (min == mid) return nums[min] < nums[max] ? max : min
+    if (min === mid) return nums[min] < nums[max] ? max : min
     min = nums[mid] < nums[mid + 1] ? mid : min
     max = nums[mid] > nums[mid + 1] ? mid : max
   }
