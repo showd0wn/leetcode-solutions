36502967dd914ab21c1ad23a84d0aa8a3a320d3e
fengyi <fengyi24@huawei.com>
2020-06-19 17:36:20 +0800

    feat: 提交解题

---
+++ algorithms/[387]字符串中的第一个唯一字符/Solution.java
@@ -0,0 +1,40 @@
+//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。 
+//
+// 
+//
+// 示例： 
+//
+// s = "leetcode"
+//返回 0
+//
+//s = "loveleetcode"
+//返回 2
+// 
+//
+// 
+//
+// 提示：你可以假定该字符串只包含小写字母。 
+// Related Topics 哈希表 字符串
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int length = s.length();
+
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (count.get(c) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
