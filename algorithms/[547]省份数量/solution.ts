bf25a5a97e11795d59997a1bad461211846bd5bc
fengyi <fengyi24@huawei.com>
2021-01-18 23:34:40 +0800

    feat: 提交解题

---
+++ algorithms/[547]省份数量/solution.ts
@@ -0,0 +1,132 @@
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+// 已知无向图的邻接矩阵，求连通分量
+function findCircleNum(isConnected: number[][]): number {
+  // dfs
+  const n = isConnected.length;
+  const visited = new Set<number>();
+  let res: number = 0;
+
+  const dfs = function (i: number): void {
+    visited.add(i);
+    for (let j = 0; j < n; j += 1) {
+      if (isConnected[i][j] === 1 && !visited.has(j)) {
+        dfs(j);
+      }
+    }
+  };
+
+  for (let i = 0; i < n; i += 1) {
+    if (!visited.has(i)) {
+      dfs(i);
+      res += 1;
+    }
+  }
+
+  return res;
+};
+
+
+function findCircleNum2(isConnected: number[][]): number {
+  // bfs
+  const n = isConnected.length;
+  const visited = new Set<number>();
+  let queue: number[] = [];
+  let res: number = 0;
+
+  for (let i = 0; i < n; i += 1) {
+    if (visited.has(i)) continue;
+
+    queue.push(i);
+    while (queue.length) {
+      const cur = queue.shift()!;
+      visited.add(cur);
+      for (let j = 0; j < n; j += 1) {
+        if (isConnected[cur][j] === 1 && !visited.has(j)) {
+          queue.push(j);
+        }
+      }
+    }
+
+    res += 1;
+  }
+
+  return res;
+};
+
+
+function findCircleNum3(isConnected: number[][]): number {
+  // Union-Find
+  const n = isConnected.length;
+  const parent = Array.from({ length: n }, (_, i) => i);
+
+  const union = function (i: number, j: number): void {
+    parent[find(i)] = find(j);
+  };
+
+  const find = function (x: number): number {
+    if (parent[x] === x) {
+      return x;
+    }
+
+    // return find(parent[x]);
+
+    // 优化(路径压缩)：把沿途的每个节点的父节点都设为根节点
+    parent[x] = find(parent[x]);
+    return parent[x];
+  };
+
+  for (let i = 0; i < n; i += 1) {
+    for (let j = i + 1; j < n; j += 1) {
+      if (isConnected[i][j] === 1) {
+        union(i, j);
+      }
+    }
+  }
+
+  return parent.filter((ele, index) => ele === index).length;
+};
+
+
+function findCircleNum4(isConnected: number[][]): number {
+  // Union-Find & 按秩合并
+  // 参考 https://zhuanlan.zhihu.com/p/93647900
+  const n = isConnected.length;
+  const parent = Array.from({ length: n }, (_, i) => i);
+  const rank = Array.from({ length: n }, () => 1);
+
+  const union = function (i: number, j: number): void {
+    const x = find(i);
+    const y = find(j);
+
+    if (rank[x] <= rank[y]) {
+      parent[x] = y;
+    } else {
+      parent[y] = x;
+    }
+
+    if (rank[x] === rank[y] && x !== y) {
+      rank[y] += 1;
+    }
+  };
+
+  const find = function (x: number): number {
+    if (parent[x] !== x) {
+      parent[x] = find(parent[x]);
+    }
+
+    return parent[x];
+  };
+
+  for (let i = 0; i < n; i += 1) {
+    for (let j = i + 1; j < n; j += 1) {
+      if (isConnected[i][j] === 1) {
+        union(i, j);
+      }
+    }
+  }
+
+  return parent.filter((ele, index) => ele === index).length;
+};
---
+++ algorithms/[721]账户合并/solution.ts
@@ -0,0 +1,86 @@
+class UnionFind {
+  parent: number[];
+  rank: number[];
+  constructor(n: number) {
+    this.parent = Array.from({ length: n }, (_, i) => i);
+    this.rank = Array.from({ length: n }, () => 1);
+  }
+  union(i: number, j: number): void {
+    const { parent, rank, find } = this;
+    const x = find(i);
+    const y = find(j);
+
+    if (rank[x] < rank[y]) {
+      parent[x] = y;
+    } else {
+      parent[y] = x;
+    }
+
+    if (rank[x] === rank[y] && x !== y) {
+      rank[y] += 1;
+    }
+  }
+  find(x: number): number {
+    const { parent, find } = this;
+
+    if (parent[x] !== x) {
+      parent[x] = find(parent[x]);
+    }
+
+    return parent[x];
+  }
+}
+
+
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+function accountsMerge(accounts: string[][]): string[][] {
+  const emailToIndex = new Map<string, number>();
+  const emailToName = new Map<string, string>();
+  let emailsCount: number = 0;
+
+  for (const account of accounts) {
+    const name = account[0];
+    const size = account.length;
+    for (let i = 1; i < size; i += 1) {
+      const email = account[i];
+      if (!emailToIndex.has(email)) {
+        emailToIndex.set(email, emailsCount);
+        emailToName.set(email, name);
+        emailsCount += 1;
+      }
+    }
+  }
+
+  const uf = new UnionFind(emailsCount);
+  for (const account of accounts) {
+    const firstEmail = account[1];
+    const firstIndex = emailToIndex.get(firstEmail)!;
+    const size = account.length;
+    for (let i = 2; i < size; i++) {
+      const nextEmail = account[i];
+      const nextIndex = emailToIndex.get(nextEmail)!;
+      uf.union(firstIndex, nextIndex);
+    }
+  }
+
+  const indexToEmails = new Map<number, string[]>();
+  for (const email of emailToIndex.keys()) {
+    const index = uf.find(emailToIndex.get(email)!);
+    const account = indexToEmails.get(index) ?? [];
+    account.push(email);
+    indexToEmails.set(index, account);
+  }
+
+  const merged = [];
+  for (const emails of indexToEmails.values()) {
+    emails.sort();
+    const name = emailToName.get(emails[0]);
+    const account = [name, ...emails];
+    merged.push(account);
+  }
+
+  return merged;
+};
