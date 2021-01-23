2bc5d65ad17f85f4f494ccbcc86af4d2e2f27112
showd0wn <showd0wn@163.com>
2021-01-24 01:24:42 +0800

    feat: 提交解题

---
+++ algorithms/[647]最长连续递增序列/solution.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n == 0 or n == 1:
+            return n
+
+        res: List[int] = [1]
+        for idx in range(1, n):
+            if nums[idx] > nums[idx - 1]:
+                res[-1] += 1
+            else:
+                res.append(1)
+
+        return max(res)
---
+++ algorithms/[647]最长连续递增序列/solution.ts
@@ -0,0 +1,19 @@
+function findLengthOfLCIS(nums: number[]): number {
+  const len = nums.length;
+
+  if (len === 0 || len === 1) {
+    return len;
+  }
+
+  let res = 0;
+  // 记录当前连续递增序列起始索引
+  let start = 0;
+  for (let i = 1; i < len; i += 1) {
+    if (nums[i] <= nums[i - 1]) {
+      res = Math.max(res, i - start);
+      start = i;
+    }
+  }
+
+  return Math.max(res, len - start);
+};
