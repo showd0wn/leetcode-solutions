03913af3fadbba48ab88f4a5cbd46030e176bb96
showd0wn <showd0wn@163.com>
2021-02-03 20:33:03 +0800

    feat: 提交解题

---
+++ algorithms/[1004]最大连续1的个数 III/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def longestOnes(self, A: List[int], K: int) -> int:
+        n = len(A)
+        count = left = right = 0
+
+        while right < n:
+            if A[right] == 1:
+                count += 1
+
+            if right - left + 1 - count > K:
+                if A[left] == 1:
+                    count -= 1
+                left += 1
+
+            right += 1
+
+        return right - left
---
+++ algorithms/[1004]最大连续1的个数 III/solution.ts
@@ -0,0 +1,20 @@
+function longestOnes(A: number[], K: number): number {
+  // 同 424
+  const n = A.length;
+  let count = 0, left = 0, right = 0;
+
+  while (right < n) {
+    if (A[right] == 1) {
+      count += 1
+    }
+    if (right - left + 1 - count > K) {
+      if (A[left] == 1) {
+        count -= 1;
+      }
+      left += 1;
+    }
+    right += 1;
+  }
+
+  return right - left;
+};
