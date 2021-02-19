d763c679cfccbff61e6f9e722830b999f941c6db
showd0wn <showd0wn@163.com>
2021-02-20 00:24:37 +0800

    feat: 提交解题

---
+++ algorithms/[119]杨辉三角 II/solution.py
@@ -0,0 +1,14 @@
+# topics = ["数组", "数学"]
+
+from typing import List
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        # 利用相邻组合数的递推关系
+        # 参考 https://leetcode-cn.com/problems/pascals-triangle-ii/solution/yang-hui-san-jiao-ii-by-leetcode-solutio-shuk/
+        res = [1] * (rowIndex + 1)
+        for i in range(1, rowIndex + 1):
+            res[i] = int(res[i - 1] * (rowIndex - i + 1) / i)
+
+        return res
---
+++ algorithms/[119]杨辉三角 II/solution.ts
@@ -0,0 +1,43 @@
+// topics = ["数组", "动态规划"]
+
+// 朴素动态规划
+function getRow1(rowIndex: number): number[] {
+  const dp: number[][] = Array.from({ length: rowIndex + 1 }, () => []);
+  dp[0] = [1];
+  dp[1] = [1, 1];
+  for (let i = 2; i <= rowIndex; i += 1) {
+    dp[i][0] = 1;
+    dp[i][i] = 1;
+    for (let j = 1; j < i; j += 1) {
+      dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+    }
+  }
+
+  return dp[rowIndex];
+}
+
+// 对第 i+1 行的计算仅用到了第 i 行的数据，可以利用滚动数组技巧，优化空间复杂度
+function getRow2(rowIndex: number): number[] {
+  let pre: number[] = [];
+  let cur: number[] = [];
+  for (let i = 0; i <= rowIndex; i += 1) {
+    cur = new Array(i + 1).fill(1);
+    for (let j = 1; j < i; j += 1) {
+      cur[j] = pre[j - 1] + pre[j];
+    }
+    pre = cur;
+  }
+  return cur;
+}
+
+// 进一步优化：从右向左遍历，仅用一个长度为 k 的一维数组
+function getRow3(rowIndex: number): number[] {
+  let res: number[] = new Array(rowIndex + 1).fill(0);
+  res[0] = 1;
+  for (let i = 0; i <= rowIndex; i += 1) {
+    for (let j = i; j >= 1; j -= 1) {
+      res[j] += res[j - 1];
+    }
+  }
+  return res;
+}
