068ee2c7ab32646642f51845ad47ccd35e244740
showd0wn <showd0wn@163.com>
2021-04-06 23:18:06 +0800

    feat: 提交解题

--- algorithms/[80]删除有序数组中的重复项 II/solution.py
+++ algorithms/[80]删除有序数组中的重复项 II/solution.py
@@ -5,15 +5,21 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        近似题 26.删除有序数组中的重复项
+        """
         n = len(nums)
+        if n <= 2:
+            return n
+
+        # 快慢指针
         i = j = 2
 
         # 当快指针遍历完数组，慢指针指向的为不重复数组结尾 + 1
         while j < n:
-            if i < 2 or nums[i - 2] != nums[j]:
+            if nums[i - 2] != nums[j]:
                 nums[i] = nums[j]
                 i += 1
-            # 出现第三个相同的元素，快指针单独前进
             j += 1
 
         return i
